65880450f2b38b30df95e729f3945bdfd8a5d960
Md. Rafiul Islam <rislamshaon95@gmail.com>
2023-10-08 21:10:55 +0600

    login and register page are designed

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,9 +1,23 @@
+import { Link } from "react-router-dom";
 
 
 const Login = () => {
     return (
-        <div>
-            
+        <div className="w-3/4">
+            <h2 className="text-3xl font-bold my-10 text-[#241468]">Please Login</h2>
+            <form>
+                <input type="email" name="email" placeholder="Your Email" id="" className="border p-4 block mb-4 w-3/4" required />
+
+                <input type="password" name="password" className="border p-4 block mb-4 w-3/4"placeholder="Password" id="" required />
+
+                <button className="btn bg-[#241468] text-white">Submit</button>
+
+                <p className="mt-3 text-xl">New to our website? Please <Link className="
+                underline text-blue-700 font-bold" to='/register'>Register</Link> </p>
+            </form>
+
+            <p className="text-xl my-3 text-blue-700">Or</p>
+            <button className="btn btn-primary mt-4">Login with Google</button>
         </div>
     );
 };
--- src/Components/Register.jsx
+++ src/Components/Register.jsx
@@ -1,8 +1,29 @@
+import { Link } from "react-router-dom";
 
 
 const Register = () => {
     return (
-        <div>
+        <div className="w-3/4">
+             <h2 className="text-3xl font-bold my-10 text-[#241468]">Please Register</h2>
+            <form>
+                <label htmlFor="name">Name</label>
+                <input type="text" name="name" id="name" className="border p-4 block my-4 w-3/4" placeholder="Your Name" required />
+
+                <label htmlFor="email">Email</label>
+                <input type="email" name="email" id="email" className="border p-4 block my-4 w-3/4" placeholder="Your Email" required/>
+
+                <label htmlFor="password">Password</label>
+                <input type="password" name="password" className="border p-4 block my-4 w-3/4" id="password" placeholder="Your Password" required/>
+
+                <label htmlFor="photo">Photo</label>
+                <input type="text" name="photoURL" id="photo" className="border p-4 block my-4 w-3/4" placeholder="Your Photo URL" />
+
+                <button className="btn bg-[#241468] text-white">Register</button>
+
+                <p className="my-3 text-xl">Already have an account? Please <Link className="
+                underline text-blue-700 font-bold" to='/login'>Login</Link> </p>
+
+            </form>
             
         </div>
     );
