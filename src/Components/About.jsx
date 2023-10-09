1fa70f44806dca0a73a20b6c9c18b2785f76aeca
Md. Rafiul Islam <rislamshaon95@gmail.com>
2023-10-09 09:01:31 +0600

    added about us page description

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -3,7 +3,17 @@
 const About = () => {
     return (
         <div>
-            
+
+            <h2 className="text-center my-10 font-bold text-4xl text-[#241468]">About Us</h2>
+            <p>At Twilight, we are more than just a birthday organizing company; we are the architects of unforgettable moments, the creators of joy, and the custodians of your most cherished memories. With a passion for celebration and an unwavering commitment to excellence, we bring your dream birthday parties to life.</p>
+
+              <p>  Our journey began with a simple belief: that every birthday should be a unique and magical experience. Since our inception, we have dedicated ourselves to crafting extraordinary celebrations that reflect your personality, style, and dreams. Whether it's a child's first birthday, a milestone event, or a surprise party for a loved one, we take pride in turning your ideas into reality.</p>
+
+             <p>   Our team of experienced professionals is at the heart of what we do. They possess the creativity, attention to detail, and organizational skills necessary to plan and execute flawless events. From venue selection and thematic decoration to entertainment and delectable catering, we handle every aspect with precision.</p>
+
+              <p>  What sets us apart is our unwavering commitment to customer satisfaction. We listen closely to your desires, work within your budget, and add our expert touch to make your birthday celebration truly exceptional.</p>
+
+              <p>  Join us at Twilight, and let us transform your special day into a remarkable memory that will last a lifetime. Because at the heart of it all, we believe that birthdays are not just milestones; they are moments to be treasured forever.</p>
         </div>
     );
 };
