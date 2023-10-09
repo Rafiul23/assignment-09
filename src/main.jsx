82da7cf377bfbcb3cf626b0e299fb7c7817af597
Md. Rafiul Islam <rislamshaon95@gmail.com>
2023-10-09 16:58:52 +0600

    added error page

--- src/main.jsx
+++ src/main.jsx
@@ -16,11 +16,13 @@ import AuthProvider from './Components/AuthProvider';
 import { ToastContainer } from 'react-toastify';
 import PrivateRoute from './PrivateRoute/PrivateRoute';
 import Contact from './Components/Contact';
+import ErrorPage from './Components/ErrorPage';
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root></Root>,
+    errorElement: <ErrorPage></ErrorPage>,
     children: [
       {
         path: "/",
